0b8fd2f28f1867ac3b5bd103dc1f4da2698421d0
shijasuresh <shija.suresh89@gmail.com>
2024-08-28 01:01:04 +1000

    Linear gradient changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0%, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%)',
+      },
+    },
   },
   plugins: [],
 }
